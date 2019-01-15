2a5dd14bcd88558060d043326b319d7be0d011f3
akashMehta-STL <akash.mehta@solutelabs.com>
2019-01-15 19:54:46 +0530

    Update theme. Add video resources.

--- lib/demo/main.dart
+++ lib/demo/main.dart
@@ -16,6 +16,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
       home: Builder(
         builder: (context) {
           return Scaffold(
